065a6d884110673ee5bd2bc8206d7e80f11663b3
AashishAdhikari27 <aashishadhikari2027@gmail.com>
2024-09-05 22:06:26 +0545

    mmr retrieval technique

--- retrieval_techniques/similarity_search.py
+++ retrieval_techniques/similarity_search.py
@@ -3,12 +3,17 @@ from langchain_community.vectorstores import Chroma
 texts = [
     """The Amanita phalloides has a large and imposing epigeous (aboveground) fruiting body (basidiocarp).""",
     """A mushroom with a large fruiting body is the Amanita phalloides. Some varieties are all-white.""",
-    """A. phalloides, a.k.a Death Cap, is one of the most poisonous of all known mushrooms.""",
+    """A. phalloides, a.k.a Death Cap, is one of the most poisonous of all known mushrooms."""
+    """ It is tasty """,
+    """Some say poisonous id tasty but it kills """,
 ]
 
 meta_data = [{"source": "document 1", "page": 1},
              {"source": "document 2", "page": 2},
-             {"source": "document 3", "page": 3},]
+             {"source": "document 3", "page": 3},
+             {"source": "document 4", "page": 4},
+             {"source": "document 5", "page": 5},]
+
 
 persist_directory = 'Documents/chroma/'
 
@@ -27,3 +32,4 @@ response = vector_db.similarity_search(
     query="Tell me about all-white mushrooms with large fruiting bodies", k=2)
 
 print(response)
+
